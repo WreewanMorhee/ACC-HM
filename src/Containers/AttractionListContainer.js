56288f205f24d66994b2bfe635b7ab3ecc848d8b
Kyle.huang <kyle.haung@hearst.com.tw>
2021-03-10 04:03:40 +0800

    replace connect to hooks

--- src/Containers/AttractionListContainer.js
+++ src/Containers/AttractionListContainer.js
@@ -1,25 +1,13 @@
-import { connect } from 'react-redux'
-import {
-  set_attraction_list,
-  set_attraction_loading,
-} from 'actions/attractionlist'
+import { ReactReduxContext } from 'react-redux'
+import { useContext } from 'react'
 import AttractionList from '../Components/AttractionList'
 
-const mapDispatchToProps = {
-  set_attraction_list,
-  set_attraction_loading,
+const AttractionListContainer = () => {
+  const dispatch = useDispatch()
+  const { store } = useContext(ReactReduxContext)
+  const { attractionList } = store.getState()
+
+  return <AttractionList dispatch={dispatch} {...attractionList} />
 }
 
-const mapStateToProps = state => {
-  const {
-    attractionList: { attraction_list, loading, should_fetch, is_end },
-  } = state
-  return {
-    should_fetch,
-    attraction_list,
-    loading,
-    is_end,
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AttractionList)
+export default AttractionListContainer
